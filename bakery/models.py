a10660140684831eaeeeccd894d3f27cdfcf0816
Ayush yadav <ayushyadav770554@gmail.com>
2026-07-05 05:01:26 +0530

    2nd

--- bakery/models.py
+++ bakery/models.py
@@ -1,4 +1,25 @@
 from django.db import models
 
 # Create your models here.
-class category(models.Model):
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    description = models.TextField()
+    name = models.CharField(max_length=100)
+    base_price = models.DecimalField(max_digits=8, decimal_places=2)
+    image = models.ImageField(upload_to='products/')
+    is_available = models.BooleanField(default=True)
+    is_featured = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
