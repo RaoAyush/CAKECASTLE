6132ae14679ca07604d4d90b52f2ba2dc7f5baf7
Ayush yadav <ayushyadav770554@gmail.com>
2026-07-06 04:03:28 +0530

    5th with description

--- bakery/urls.py
+++ bakery/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns=[
     path('',views.home, name='home'),
+    path('about/',views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('menu/',views.menu, name='menu'),
+    path('product/<int:product_id>/', views.product_detail, name='product_detail'),
 ]
--- bakery/views.py
+++ bakery/views.py
@@ -1,8 +1,32 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import render, get_object_or_404
+from .models import Product , Category
 
 def home(request):
     products=Product.objects.filter(is_available=True)
     return render(request, 'bakery/home.html',{'products': products})
+  
+def about(request):
+    return render(request, 'bakery/about.html')
 
-# Create your views here.
+def contact(request):
+    return render(request, 'bakery/contact.html')
+
+def menu(request):
+    categories = Category.objects.all()
+    selected_category = request.GET.get('category')
+
+    if selected_category:
+        products = Product.objects.filter(is_available=True, category__slug=selected_category)
+    else:
+        products = Product.objects.filter(is_available=True)
+
+    context = {
+        'categories': categories,
+        'products': products,
+        'selected_category': selected_category,
+    }
+    return render(request, 'bakery/menu.html', context)
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, id=product_id, is_available=True)
+    return render(request, 'bakery/product_detail.html', {'product': product})
